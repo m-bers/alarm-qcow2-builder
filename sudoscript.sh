bf04eeaebc4f6130dd549c0ccf5cc27f483b5bbc
Josh Chambers <joshch@mbe.rs>
2021-03-27 20:18:41 -0700

    UUIDs

--- sudoscript.sh
+++ sudoscript.sh
@@ -3,12 +3,15 @@
 set -x
 
 echo -e "g\nn\n\n\n+200M\nt\n1\nn\n\n\n\nw\n" | fdisk archlinux.img
-read -r -d '\n' ARCHBOOT ARCHROOT <<<$(kpartx -av archlinux.img | grep -oh "\w*loop\w*")
-mkfs.vfat /dev/mapper/$ARCHBOOT
-mkfs.ext4 /dev/mapper/$ARCHROOT
+read -r -d '\n' ARCHBOOT ARCHROOT <<<"/dev/mapper/$(kpartx -av archlinux.img | grep -oh "\w*loop\w*")"
+mkfs.vfat $ARCHBOOT
+mkfs.ext4 $ARCHROOT
 mkdir root
-mount /dev/mapper/$ARCHROOT root
+mount $ARCHROOT root
 mkdir root/boot
-mount /dev/mapper/$ARCHBOOT root/boot
+mount $ARCHBOOT root/boot
 bsdtar -xpf ArchLinuxARM-aarch64-latest.tar.gz -C root
-blkid
+BOOTUUID=$(blkid $ARCHBOOT -o value -s UUID)
+ROOTUUID=$(blkid $ARCHROOT -o value -s UUID)
+echo "BOOTUUID=$BOOTUUID"
+echo "ROOTUUID=$ROOTUUID"
