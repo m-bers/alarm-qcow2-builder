4b822bd2ba544fc75c6b04a4feba087b41226531
Josh Chambers <joshch@mbe.rs>
2021-03-27 18:30:05 -0700

    changes

--- script.sh
+++ script.sh
@@ -4,3 +4,13 @@ set -x
 
 sudo apt update
 sudo apt -y install qemu-system-arm qemu-efi-aarch64 qemu-utils
+dd if=/dev/zero of=flash1.img bs=1M count=64
+dd if=/dev/zero of=flash0.img bs=1M count=64
+dd if=/usr/share/qemu-efi-aarch64/QEMU_EFI.fd of=flash0.img conv=notrunc
+wget http://ports.ubuntu.com/ubuntu-ports/dists/bionic-updates/main/installer-arm64/current/images/netboot/mini.iso
+qemu-img create -f qcow2 ubuntu-image.qcow2 20G
+qemu-system-aarch64 -nographic -machine virt,gic-version=max -m 512M -cpu max -smp 4 \
+    -netdev user,id=vnet,hostfwd=:127.0.0.1:0-:22 -device virtio-net-pci,netdev=vnet \
+    -drive file=ubuntu-image.qcow2,if=none,id=drive0,cache=writeback -device virtio-blk,drive=drive0,bootindex=0 \
+    -drive file=mini.iso,if=none,id=drive1,cache=writeback -device virtio-blk,drive=drive1,bootindex=1 \
+    -drive file=flash0.img,format=raw,if=pflash -drive file=flash1.img,format=raw,if=pflash 
