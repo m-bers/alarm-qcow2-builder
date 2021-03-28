e97dfdcf3a91a5baaf9043746c6d2dbab26b7423
Josh Chambers <joshch@mbe.rs>
2021-03-27 20:23:22 -0700

    variables

--- sudoscript.sh
+++ sudoscript.sh
@@ -3,7 +3,8 @@
 set -x
 
 echo -e "g\nn\n\n\n+200M\nt\n1\nn\n\n\n\nw\n" | fdisk archlinux.img
-read -r -d '\n' ARCHBOOT ARCHROOT <<<"/dev/mapper/$(kpartx -av archlinux.img | grep -oh "\w*loop\w*")"
+read -r -d '\n' ARCHBOOT ARCHROOT <<<"$(kpartx -av archlinux.img | grep -oh "\w*loop\w*")"
+read -r ARCHBOOT ARCHROOT <<<"/dev/mapper/${ARCHBOOT} /dev/mapper/${ARCHROOT}"
 mkfs.vfat $ARCHBOOT
 mkfs.ext4 $ARCHROOT
 mkdir root
