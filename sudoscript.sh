07ffd8e90de76cf13cc5e324dbbd37735998e437
Josh Chambers <joshch@mbe.rs>
2021-03-27 20:10:08 -0700

    add full path for loop devices

--- sudoscript.sh
+++ sudoscript.sh
@@ -3,12 +3,12 @@
 set -x
 
 echo -e "g\nn\n\n\n+200M\nt\n1\nn\n\n\n\nw\n" | fdisk archlinux.img
-read -r -d '\n' ARCHBOOT ARCHROOT <<<$(sudo kpartx -av archlinux.img | grep -oh "\w*loop\w*")
-mkfs.vfat $ARCHBOOT
-mkfs.ext4 $ARCHROOT
+read -r -d '\n' ARCHBOOT ARCHROOT <<<$(kpartx -av archlinux.img | grep -oh "\w*loop\w*")
+mkfs.vfat /dev/mapper/$ARCHBOOT
+mkfs.ext4 /dev/mapper/$ARCHROOT
 mkdir root
-mount $ARCHROOT root
+mount /dev/mapper/$ARCHROOT root
 mkdir root/boot
-mount $ARCHBOOT root/boot
+mount /dev/mapper/$ARCHBOOT root/boot
 bsdtar -xpf ArchLinuxARM-aarch64-latest.tar.gz -C root
 blkid
