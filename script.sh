74cc73ed4c42a5d9d0404fc688f5a38532bb25f6
Josh Chambers <joshch@mbe.rs>
2021-03-27 18:52:28 -0700

    fixes

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 set -x
 
 sudo apt update
-sudo apt -y install qemu-system-arm qemu-efi-aarch64 qemu-utils fdisk kpartx bsdtar wget
+sudo apt -y install qemu-system-arm qemu-efi-aarch64 qemu-utils fdisk kpartx libarchive-tools wget
 mkdir alarm
 cd alarm
 wget http://os.archlinuxarm.org/os/ArchLinuxARM-aarch64-latest.tar.gz
