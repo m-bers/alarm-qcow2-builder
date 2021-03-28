ec86a218af1a39141715777d9c37031e00055390
Josh Chambers <joshch@mbe.rs>
2021-03-27 19:10:48 -0700

    revert

--- script.sh
+++ script.sh
@@ -3,11 +3,10 @@
 set -x
 
 sudo apt update
-sudo apt -y install qemu-system-arm qemu-efi-aarch64 qemu-utils fdisk kpartx libarchive-tools wget
+sudo apt -y install qemu-system-arm qemu-efi-aarch64 qemu-utils fdisk kpartx bsdtar wget
 mkdir alarm
 cd alarm
 wget http://os.archlinuxarm.org/os/ArchLinuxARM-aarch64-latest.tar.gz
-qemu-img create archlinux.img 32G
-echo -e "g\nn\n\n\n+200M\nt\n1\nn\n\n\nw\n" | fdisk archlinux.img
-sudo kpartx -av archlinux.img
-ls /dev/mapper
+qemu-img create archlinux.img 32GB
+echo -e "g\nn\n\n\n+200M\nt\n1\nn\n\n\n\nw\n" | fdisk $DISK
+sudo
